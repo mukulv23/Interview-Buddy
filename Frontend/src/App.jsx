18cbf6cd0873621a87d1ef490cd2158a9b39a70d
Mukul Verma <mukulv072@gmail.com>
2026-06-15 13:15:34 +0530

    Made Navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,10 @@
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-blue-500">
-      Tailwind CSS v3 Working!
-    </h1>
+    <header>
+      <Navbar/>
+    </header>
   );
 }
 
---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { Bot, Menu, X } from 'lucide-react'
+import { NavLink, useNavigate } from 'react-router-dom'
+export const Navbar = () => {
+
+    const [show, setShow] = useState(false)
+    const navigate = useNavigate();
+
+    return (
+        <nav className='bg-white shadow pt-6'>
+            <div className='max-w-7xl mx-auto px-4'>
+                <div className='flex justify-between items-center h-16'>
+                    <div className='flex gap-4 items-center md:cursor-pointer' onClick={() => {
+                        navigate('/');
+                    }}>
+                        <div className='rounded-xl p-2 bg-[#f1ecfe]'>
+                            <Bot size={16} className='text-[#a16efe]' />
+                        </div>
+                        <span className='font-bold text-lg'>Interview Buddy</span>
+                    </div>
+                    {show ?
+                        <ul className="flex flex-col items-center absolute top-23 right-0 w-36 bg-white shadow-lg rounded-xl p-4 gap-4 transition duration-200 z-10 md:hidden">
+                            <NavLink to="/create-interview" className="text-lg font-medium">Interview</NavLink>
+                            <NavLink to="/wallet" className="text-lg font-medium">Wallet</NavLink>
+                            <NavLink to="/profile" className="text-lg font-medium">Profile</NavLink>
+                        </ul> :
+                        <ul className='hidden md:flex items-center gap-6'>
+                            <NavLink to="/create-interview" className="text-lg font-medium border-b-2 border-transparent hover:border-b-lime-950">Interview</NavLink>
+                            <NavLink to="/wallet" className="text-lg font-medium border-b-2 border-transparent hover:border-b-lime-950">Wallet</NavLink>
+                            <NavLink to="/profile" className="text-lg font-medium border-b-2 border-transparent hover:border-b-lime-950">Profile</NavLink>
+                        </ul>
+                    }
+                    <button className='md:hidden' onClick={() => setShow(!show)}>
+                        {show ? <X /> : <Menu />}
+                    </button>
+                </div>
+            </div>
+        </nav>
+    )
+}
