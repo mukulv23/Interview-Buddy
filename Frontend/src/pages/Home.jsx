584fff4c71b9c547b1c733f431e481635cef25ed
Mukul Verma <mukulv072@gmail.com>
2026-07-11 12:24:51 +0530

    checking deployment

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -44,7 +44,7 @@ export const Home = () => {
                     <h1 className='text-3xl sm:text-4xl max-w-full md:max-w-125 font-bold text-slate-900 leading-tight mb-2'>Ace Your Next Interview</h1>
                     <span className='text-[#566476] max-w-full md:max-w-135 mb-2 leading-6'>Practice with AI-powered mock interviews and land your dream job.</span>
 
-                    <div className="flex flex-col sm:flex-row gap-3 mt-6">
+                    {/* <div className="flex flex-col sm:flex-row gap-3 mt-6">
                         <button className="bg-[#13A594] cursor-pointer border border-[#13A594] hover:bg-[#13A594]/90 text-white text-sm font-semibold rounded-md px-5 py-2.5">
                             Manage cookie settings
                         </button>
@@ -52,9 +52,9 @@ export const Home = () => {
                         <button className="bg-white border font-semibold cursor-pointer border-[#CDD7E3] text-[#0D1B2E] hover:bg-white/10 text-sm rounded-md px-5 py-2.5">
                             Privacy Center
                         </button>
-                    </div>
+                    </div> */}
 
-                    <a
+                    {/* <a
                         href="#"
                         className="mt-4 inline-block text-sm font-medium text-[#13A594] hover:underline"
                     >
@@ -67,7 +67,7 @@ export const Home = () => {
                             alt="image"
                             height={15}
                             width={15}
-                            className="mt-1 bg-red-100 shrink-0"
+                            className="mt-1 bg-red shrink-0"
                         />
                         <span>
                             Essential cookies keep the site working. Non-essential cookies
@@ -90,7 +90,7 @@ export const Home = () => {
                             <RocketIcon size={16} />
                             <span className='font-bold'>Get Started</span>
                         </button>}
-                </div>
+                </div> */}
             </div>
         </section>
     )
