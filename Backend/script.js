7306ec7596b3864b58657133192002c2ad0741cf
Mukul Verma <mukulv072@gmail.com>
2026-07-11 12:38:40 +0530

    Cors issue resolving

--- Backend/script.js
+++ Backend/script.js
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(cors({
     origin: [
-        "http://localhost:5173/",
+        "http://localhost:5173",
         process.env.CLIENT_URL,
     ],
     credentials: true
